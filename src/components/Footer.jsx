0853c808d54bb2d7e0c368b771310d070d0e48e5
Man-Q <81616738+Man-Q@users.noreply.github.com>
2024-06-09 19:10:08 +0200

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Footer(){
+    
+    return(
+        <div className="footer">
+            <p><span>&#169;</span>2022 #VANLIFE</p>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+
+export default function Header(){
+    
+    return(
+        <header className="header nav">
+            <div className="site-logo">
+                <h2>#VanLife</h2>
+            </div>
+            <div id="sub-header">
+                <div>
+                    <a href="#"><h4>About</h4></a>
+                </div>
+                <div>
+                    <a href="#"><h4>Vans</h4></a>
+                </div>
+            </div>
+        </header>
+    )
+}
