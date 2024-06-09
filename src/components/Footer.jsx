73e890e2e6aacfc5221b4f0df1700acb2cfe2390
Man-Q <81616738+Man-Q@users.noreply.github.com>
2024-06-09 19:51:28 +0200

    Footer refinment.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@
 export default function Footer(){
     
     return(
-        <div className="footer">
+        <footer className="footer">
             <p><span>&#169;</span>2022 #VANLIFE</p>
-        </div>
+        </footer>
     )
-    
+
 }
